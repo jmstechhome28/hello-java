e4564783ff9d699b4445c3a6ca6287e50e0bc36a
jmstechhome28 <jmstechhome28@gmail.com>
2024-07-04 01:39:00 +0000

    added first hello java file

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+    System.out.println("this is madhu");
+    System.out.println("HI 43 batch devops");
+  }
+}
