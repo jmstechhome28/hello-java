d593bd319a5fa74d9e907e2c22ada285a35a81b2
jmstechhome28 <jmstechhome28@gmail.com>
2024-07-05 07:29:47 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("this is madhu");
-    System.out.println("this is madhu");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
+
   }
 }
